0882767a42604572f524f58927c7a371efc85ed8
Joonho Han <joonhojhan@gmail.com>
2021-11-16 15:56:07 -0500

    updates db

--- db/db.js
+++ db/db.js
@@ -4,20 +4,21 @@ const databaseName = process.env.DB_NAME || 'zhaul'
 
 let db
 
-// if (process.env.DATABASE_URL) {
-//   db = new Sequelize(process.env.DATABASE_URL, { logging: false })
-// } else {
-db = new Sequelize(
-  process.env.DATABASE,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false,
-  }
-)
-// }
+if (process.env.DATABASE_URL) {
+  db = new Sequelize(process.env.DATABASE_URL, { logging: false })
+} else {
+  db = new Sequelize(
+    process.env.DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'postgres',
+      logging: false,
+      ssl: true,
+    }
+  )
+}
 
 module.exports = db
