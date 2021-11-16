accdb4982240a170c506cbd52ba85e5c77b4fcd7
Joonho Han <joonhojhan@gmail.com>
2021-11-16 15:47:51 -0500

    updates db

--- db/db.js
+++ db/db.js
@@ -2,9 +2,21 @@ const Sequelize = require('sequelize')
 
 const databaseName = process.env.DB_NAME || 'zhaul'
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  { logging: false }
+let db
+
+// if (process.env.DATABASE_URL) {
+//   db = new Sequelize(process.env.DATABASE_URL, { logging: false })
+// } else {
+db = new Sequelize(
+  process.env.DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    logging: false,
+  }
 )
+// }
 
 module.exports = db
